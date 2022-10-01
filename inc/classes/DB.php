57add2fdfede6d80d35f4e63e6cedfc444d457e8
xkas01 <xkas2001@gmail.com>
2022-10-01 08:51:15 +0500

    Filled DB class with some functions and connect DB

--- inc/classes/DB.php
+++ inc/classes/DB.php
@@ -1,1 +1,36 @@
 <?php
+
+if (!defined('__CONFIG__')) {
+    exit('Access Denied');
+}
+
+class DB
+{
+    public $host = 'localhost';
+    public $port = '3306';
+    public $name = 'tutorial_rest';
+    public $user = 'ata';
+    public $password = '123456';
+
+    protected static $con;
+
+    private function __construct()
+    {
+        try {
+            self::$con = new PDO('mysql:charset=utf8mb4;host=' . ($this->host) . ';port=' . ($this->port) . ';dbname=' . ($this->name), $this->user, $this->password);
+            self::$con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            self::$con->setAttribute(PDO::ATTR_PERSISTENT, false);
+        } catch (PDOException $e) {
+            print "Could not connect to database.";
+            exit();
+        }
+    }
+
+    public function getConnection()
+    {
+        if (!self::$con) {
+            new DB();
+        }
+        return self::$con;
+    }
+}
--- inc/config.php
+++ inc/config.php
@@ -4,11 +4,14 @@ if (!defined('__CONFIG__')) {
     exit('Access Denied');
 }
 
+//Allways set default_timezone
 date_default_timezone_set("Asia/Tashkent");
 
+//Allow errors
 error_reporting(1);
 ini_set('display_errors', 'On');
 
+//Include classes
 include_once "classes/DB.php";
 include_once "classes/Filter.php";
 include_once "classes/Request.php";
